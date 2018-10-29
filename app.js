2f760a32ad5124e6de50667a21c38d617bb37f70
ghimiremanish <manishghimire153200@gmail.com>
2018-10-29 21:50:47 +0545

    fixing errors

--- app.js
+++ app.js
@@ -20,7 +20,8 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Methods','PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
     }
-})
+    next();
+});
 
 // Routes which handels request
 app.use('/products', productRoutes);
