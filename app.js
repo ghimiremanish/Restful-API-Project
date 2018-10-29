6875b6fbec4fe2734baedb86bd254a555a9e9ce9
ghimiremanish <manishghimire153200@gmail.com>
2018-10-29 14:29:51 +0545

    changing package . json file to work with nodemon

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-
+// Routes which handels request
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
