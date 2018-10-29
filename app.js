47387b63e37bf72a3df8e6ef76459edd872cb7ac
ghimiremanish <manishghimire153200@gmail.com>
2018-10-29 14:33:46 +0545

    morgan the log manager package added

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');// log manager
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 // Routes which handels request
 app.use('/products', productRoutes);
