75c4e0767e8cc8bebff0afe2be9a888311cffdec
ghimiremanish <manishghimire153200@gmail.com>
2018-10-29 14:00:40 +0545

    routes making

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,4 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/')
+router.get('/', (req,res,next) => {
+    res.status(200).json({
+        message:'Handling Get Request to /products'
+    });
+});
+
+
+router.post('/', (req,res,next) => {
+    res.status(200).json({
+        message:'Handling POST Request to /products'
+    });
+});
+
+router.delete('/', (req,res,next) => {
+    res.status(200).json({
+        message:'Handling Delete Request to /products'
+    });
+});
+
+
+router.get('/:productID', (req, res, next) => {
+    const id = req.params.productID;
+    if (id === 'status') {
+        res.status(200).json({
+            message: 'hello manish',
+            id: id
+        });
+    } else{
+        res.status(200).json({
+            id: id
+        });
+    }
+});
+module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-  res.status(200).json({
-    message: "It's works"
-  });
-});
+const productRoutes = require('./api/routes/products');
+
+app.use('/products', productRoutes);
+
+
+
 module.exports = app;
