380a42b73755cd1e60c70736e5cf68c4d79cf0f9
ghimiremanish <manishghimire153200@gmail.com>
2018-10-29 14:46:00 +0545

    error handling success

--- app.js
+++ app.js
@@ -11,5 +11,20 @@ app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+//error handling whild typying unknown URL
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
