6e6d9298f9274deace0e158b7265ee42fa09606f
ghimiremanish <manishghimire153200@gmail.com>
2018-10-29 15:00:08 +0545

    parsing handling using body parser

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');// log manager
+const bodyParser = require('body-parser'); // package which help to parse incoming data
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); //this will extract data & show in easily readable format
 
 // Routes which handels request
 app.use('/products', productRoutes);
